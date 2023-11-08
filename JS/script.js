bf6d06b42b1f52becf2b40e2dc73845068de4f71
carlomarcon <carlomarcon1998@gmail.com>
2023-11-08 15:36:13 +0100

    ADD: FIRST EXERCISE

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const arrayBike = [
+  {
+    nome: "Super",
+    peso: 20,
+  },
+  {
+    nome: "great",
+    peso: 10,
+  },
+  {
+    nome: "fast",
+    peso: 5,
+  },
+];
+
+console.log(arrayBike[2].peso);
---
+++ readme.md
@@ -0,0 +1,3 @@
+<!-- 2 ESERCIZIO -->
+
+### RACCOGLIERE RISORSE
